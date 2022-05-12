6563183eaafd987b3989705870198baf41de2e30
alexey.orlov <pope.orlov@gmail.com>
2022-05-12 13:24:28 +0300

    Практическая работа. Работа с большими и точными числами (автотесты) Грузовики

--- java_basics/07_NumbersAndDates/GeometryCalculator/src/main/java/Main.java
+++ java_basics/07_NumbersAndDates/GeometryCalculator/src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
         GeometryCalculator.getCircleSquare(2.0);
         GeometryCalculator.getSphereVolume(4.0);
         GeometryCalculator.isTrianglePossible(3, 2, 3);
-        GeometryCalculator.getTriangleSquare(3, 2, 3);
+        GeometryCalculator.getTriangleSquare(3, 4, 5);
 
 
     }
--- java_basics/07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ java_basics/07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -2,13 +2,51 @@ import java.util.Scanner;
 
 public class Main {
 
+
+    private static int containerInTruck = 12;
+    private static int boxInContainer = 27;
+    private static int truck = 1;
+    private static int container = 1;
+    private static int box = 1;
+
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
+        int box = Integer.parseInt(boxes);
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+        if (box > 0) {
+            System.out.println("Грузовиков: " + truck);
+            System.out.println("\tКонтейнеров: " + container);
+        }
+        for (int i = 1; i <= box; i++) {
+            System.out.println("\t\tящиков: " + i);
+
+            if (i % (boxInContainer * containerInTruck) == 0) {
+                truck++;
+                System.out.println("Грузовиков: " + truck);
+
+            }
+            if (i % boxInContainer == 0) {
+                container++;
+                System.out.println("\tКонтейнеров: " + container);
+
+
+
+
+            }
+
+        }
+        System.out.println("Необходимо Грузовиков: " + truck);
+        System.out.println("Необходимо Контейнеров: " + container);
+    }
+
+}
+
+// TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
+// пример вывода при вводе 2
+// для отступа используйте табуляцию - \t
 
         /*
         Грузовик: 1
@@ -19,6 +57,6 @@ public class Main {
         грузовиков - 1 шт.
         контейнеров - 1 шт.
         */
-    }
 
-}
+
+
