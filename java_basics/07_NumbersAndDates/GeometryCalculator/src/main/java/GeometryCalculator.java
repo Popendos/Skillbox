4691e4f10a5094f112e33a31492641066869690b
alexey.orlov <pope.orlov@gmail.com>
2022-05-12 09:48:47 +0300

    Практическая работа. Работа с большими и точными числами (автотесты)

--- java_basics/07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ java_basics/07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -1,21 +1,55 @@
 public class GeometryCalculator {
+
+    private static double S;
+    private static double V;
+    private static boolean trianglePossible;
+
     // метод должен использовать абсолютное значение radius
     public static double getCircleSquare(double radius) {
+        if (radius > 0) {
+            S = Math.PI * Math.pow(radius, 2);
+            System.out.println("Площадь круга = " + S);
+        } else {
+            System.out.println("Радиус меньше нуля");
+
+
+        }
         return 0;
     }
 
+
     // метод должен использовать абсолютное значение radius
     public static double getSphereVolume(double radius) {
+        if (radius > 0) {
+            V = (double) (4 / 3) * Math.PI * Math.pow(radius, 3);
+            System.out.println("Объем шара равен: " + V);
+        } else {
+            System.out.println("Радиус меньше нуля");
+        }
         return 0;
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
+        if ((a + b) > c && (a + c) > b && (b + c) > a) {
+            trianglePossible = true;
+            System.out.println("Возможно построить");
+
+        } else System.out.println("Невозможно построить такой треугольник");
         return false;
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
     // методом isTrianglePossible, если невозможен вернуть -1.0
     public static double getTriangleSquare(double a, double b, double c) {
-        return 0;
+        double p;
+        double S;
+        if (trianglePossible = true) {
+            p = (a + b + c) / 2;
+            S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+            System.out.println(S);
+
+
+        } else System.out.println("Невозможно построить такой треугольник");
+        return -1;
     }
 }
--- java_basics/07_NumbersAndDates/GeometryCalculator/src/main/java/Main.java
+++ java_basics/07_NumbersAndDates/GeometryCalculator/src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
+        GeometryCalculator.getCircleSquare(2.0);
+        GeometryCalculator.getSphereVolume(4.0);
+        GeometryCalculator.isTrianglePossible(3, 2, 3);
+        GeometryCalculator.getTriangleSquare(3, 2, 3);
+
+
     }
 }
