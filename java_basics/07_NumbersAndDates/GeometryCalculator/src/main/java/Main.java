2da39dd0d0e826bc5f0dcb7cbccb0cf53c078498
alexey.orlov <pope.orlov@gmail.com>
2022-05-13 08:52:06 +0300

    все ебучие тесты ебучего калькулятора прошли!!! шатал иванович!!!

--- java_basics/07_NumbersAndDates/GeometryCalculator/src/main/java/Main.java
+++ java_basics/07_NumbersAndDates/GeometryCalculator/src/main/java/Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        GeometryCalculator.getCircleSquare(2.0);
-        GeometryCalculator.getSphereVolume(4.0);
-        GeometryCalculator.isTrianglePossible(3, 2, 3);
         GeometryCalculator.getTriangleSquare(3, 4, 5);
+        GeometryCalculator.isTrianglePossible(-3, 4, 5);
+
+
 
 
     }
