81372cb498c4251846669852d894556e0e57dfcd
alexey.orlov <pope.orlov@gmail.com>
2022-05-13 12:23:03 +0300

    А я сделал цикл на дни рождения!!!!! И тестики проверил!

--- java_basics/07_NumbersAndDates/Date/src/main/java/Main.java
+++ java_basics/07_NumbersAndDates/Date/src/main/java/Main.java
@@ -4,15 +4,11 @@ import java.util.Locale;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
-
-        int day = 31;
-        int month = 12;
+        int day = 13;
+        int month = 04;
         int year = 1990;
-
-
 
         System.out.println(collectBirthdays(year, month, day));
 
@@ -22,27 +18,26 @@ public class Main {
         LocalDate birthday = LocalDate.of(year, month, day);
         LocalDate today = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E", Locale.ENGLISH);
-        int number = 0;
+
         String text = "";
 
-        if (birthday.isBefore(today)) {
-            text = text + number + " - " + formatter.format(birthday) + System.lineSeparator();
-            number++;
-            birthday = birthday.plusYears(1);
+        int i = 0;
 
-        } if (birthday.isEqual(today)) {
-            text = text + number + " - " + formatter.format(birthday);
+        while (true) {
+            if (birthday.isAfter(today)) {
+                break;
+            } else {
+                text = text + i++ + " - " + birthday.format(formatter) + System.lineSeparator();
+                birthday = birthday.plusYears(1);
+            }
         }
-
-
-
-
-
-
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
         return text;
+
+
     }
 }
+
+
+
+
+
