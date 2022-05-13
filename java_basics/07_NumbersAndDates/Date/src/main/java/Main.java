af8a62397908a88047d51a1949d77d41e793d047
alexey.orlov <pope.orlov@gmail.com>
2022-05-13 09:26:32 +0300

    Тесты на дни рождения

--- java_basics/07_NumbersAndDates/Date/src/main/java/Main.java
+++ java_basics/07_NumbersAndDates/Date/src/main/java/Main.java
@@ -1,11 +1,12 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
 
         int day = 31;
         int month = 12;
@@ -20,6 +21,18 @@ public class Main {
     public static String collectBirthdays(int year, int month, int day) {
         LocalDate birthday = LocalDate.of(year, month, day);
         LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E", Locale.ENGLISH);
+        int number = 0;
+        String text = "";
+
+        if (birthday.isBefore(today)) {
+            text = text + number + " - " + formatter.format(birthday) + System.lineSeparator();
+            number++;
+            birthday = birthday.plusYears(1);
+
+        } if (birthday.isEqual(today)) {
+            text = text + number + " - " + formatter.format(birthday);
+        }
 
 
 
@@ -30,6 +43,6 @@ public class Main {
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
         
-        return "";
+        return text;
     }
 }
