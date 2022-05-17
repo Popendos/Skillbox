c6c9c0830945b7e083ac5dde24c28d05c4abe6ca
alexey.orlov <pope.orlov@gmail.com>
2022-05-17 12:15:56 +0300

    Расчет даты 1990 до сегодняшнего дня

--- java_basics/07_NumbersAndDates/Periods/src/main/java/Main.java
+++ java_basics/07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,26 @@
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        LocalDate firstDate = LocalDate.of(1990, 02, 25);
+        LocalDate secondDate = LocalDate.now();
+        System.out.println(getPeriodFromBirthday(firstDate, secondDate));
 
-  }
 
-  private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
-  }
+    }
+
+
+    public static String getPeriodFromBirthday(LocalDate firstDate, LocalDate secondDate) {
+
+
+
+       Period period = Period.between(firstDate, secondDate);
+return "years: " + period.getYears() + " Month: " + period.getMonths() + " Days: " + period.getDays();
+    }
+
 
 }
